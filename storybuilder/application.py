c69c88a88bee9c746470624ef5fc44082967519d
nagisc007 <nagisc007@yahoo.co.jp>
2021-02-11 16:54:47 +0900

    Added: application

---
+++ storybuilder/application.py
@@ -0,0 +1,21 @@
+"""Main application for storybuilder."""
+
+
+# Alias
+StatusCode = int
+
+
+class Application(object):
+
+    """Application for storybuilder."""
+
+    def __init__(self):
+        # base setting
+        pass
+
+    # main method
+    def run(self) -> StatusCode:
+        exit_code = 0 # 0 is true
+
+        return exit_code
+
