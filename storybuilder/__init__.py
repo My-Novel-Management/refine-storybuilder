58d939819d3ea14c6c83e997fe99e6fd5a55792b
nagisc007 <nagisc007@yahoo.co.jp>
2021-02-12 15:48:07 +0900

    Added: base path

--- storybuilder/__init__.py
+++ storybuilder/__init__.py
@@ -1,6 +1,19 @@
 """Main StoryBuilder Package"""
 
+# official libraries
+import os
+
 
 # Define public constants
 __version__ = "0.0.1"
+"""str: this application version number."""
+
+HOME = os.environ['HOME']
+"""str: path of user home directory."""
+
+USR_CACHE_DIR = os.path.join(HOME, '.cache')
+"""str: path of user cache directory."""
+
+USR_CONFIG_DIR = os.path.join(HOME, '.config')
+"""str: path of user config directory."""
 
