1214cd395b8147346d48633f3398e81c8fa5089e
nagisc007 <nagisc007@yahoo.co.jp>
2021-02-20 11:30:20 +0900

    Added: is_dict, is_list

--- storybuilder/util/assertion.py
+++ storybuilder/util/assertion.py
@@ -7,6 +7,22 @@ __all__ = (
         )
 
 
+def is_dict(val: dict) -> dict:
+    """Assertion for a dictionary type value.
+
+    Args:
+        val: check value (expect dict type)
+
+    Returns:
+        Same value to input.
+
+    Raises:
+        Assertion Error: if a value not match a dict type.
+    """
+    assert isinstance(val, dict), f"Must be a Dioctonary type value!: {type(val)}"
+    return val
+
+
 def is_int(val: int) -> int:
     """Assertion for an integer type value.
 
@@ -20,6 +36,22 @@ def is_int(val: int) -> int:
         Assertion Error: if a value not match an integer type.
     """
     assert isinstance(val, int), f"Must be a Integer type value!: {type(val)}"
+    return val
+
+
+def is_list(val: list) -> list:
+    """Assertion for a list type value.
+
+    Args:
+        val: check value (expect list type)
+
+    Returns:
+        Same value to input.
+
+    Raises:
+        Assertion Error: if a value not match a list type.
+    """
+    assert isinstance(val, list), f"Must be a List type value!: {type(val)}"
     return val
 
 
