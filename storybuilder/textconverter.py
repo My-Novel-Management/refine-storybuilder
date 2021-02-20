1ebef6dece0ad87656416433c62de85eae8d7ece
nagisc007 <nagisc007@yahoo.co.jp>
2021-02-20 11:33:34 +0900

    Added: converter

---
+++ storybuilder/textconverter.py
@@ -0,0 +1,17 @@
+"""Utility module for text convert."""
+
+# official libraries
+import re
+
+# my modules
+
+
+class TextConverter(object):
+
+    """Utility module for story output."""
+
+    def __init__(self):
+        pass
+
+    # methods
+
