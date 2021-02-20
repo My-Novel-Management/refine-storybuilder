ddb7bcf7838b63255b74bc4ef6f0294d9b0fa079
nagisc007 <nagisc007@yahoo.co.jp>
2021-02-20 11:30:09 +0900

    Added: formatter (empty methods)

---
+++ storybuilder/formatter.py
@@ -0,0 +1,33 @@
+"""Format module for storybuilder output."""
+
+# official libraries
+
+
+# my modules
+from storybuilder.util.log import logger
+
+
+class StoryFormatter(object):
+
+    """Format module for storybuilder."""
+
+    def __init__(self):
+        pass
+
+    # methods
+    def conv_outline_format(self) -> list:
+        tmp = []
+        return tmp
+
+    def conv_plot_format(self) -> list:
+        tmp = []
+        return tmp
+
+    def conv_script_format(self) -> list:
+        tmp = []
+        return tmp
+
+    def conv_novel_format(self) -> list:
+        tmp = []
+        return tmp
+
