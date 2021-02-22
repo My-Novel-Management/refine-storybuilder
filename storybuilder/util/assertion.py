6ea81fbfc43b5e3f590b181c37a6f7df62cbc858
nagisc007 <nagisc007@yahoo.co.jp>
2021-02-22 10:42:42 +0900

    Added: is_instance, is_dict and is_list test

--- storybuilder/util/assertion.py
+++ storybuilder/util/assertion.py
@@ -1,9 +1,16 @@
 """Utility module for assertion."""
 
+# official libraries
+from typing import T
+
 
 __all__ = (
+        'is_dict',
+        'is_instance',
         'is_int',
+        'is_list',
         'is_str',
+        'is_tuple',
         )
 
 
@@ -20,6 +27,22 @@ def is_dict(val: dict) -> dict:
         Assertion Error: if a value not match a dict type.
     """
     assert isinstance(val, dict), f"Must be a Dioctonary type value!: {type(val)}"
+    return val
+
+
+def is_instance(val: T, a_type: T) -> T:
+    """Assertion for any type value (e.g. Class instance, data class)
+
+    Args:
+        val: check value (expect object type)
+
+    Returns:
+        Same value to input.
+
+    Raises:
+        Assertion Error: if a value not match the type.
+    """
+    assert isinstance(val, a_type), f"Must be a {a_type} value!: {type(val)}"
     return val
 
 
@@ -68,5 +91,21 @@ def is_str(val: str) -> str:
         Assertion Error: if a value not match a string type.
     """
     assert isinstance(val, str), f"Must be a String type value!: {type(val)}"
+    return val
+
+
+def is_tuple(val: tuple) -> tuple:
+    """Assertion for a tuple type value.
+
+    Args:
+        val: check value (expect tuple type)
+
+    Returns:
+        Same value to input.
+
+    Raises:
+        Assertion Error: if a value not match a tuple type.
+    """
+    assert isinstance(val, tuple), f"Must be a Tuple type value!: {type(val)}"
     return val
 
